c494ca1f11000905b5595fcb02eb46780cd9bb67
murrie030 <43439571+murrie030@users.noreply.github.com>
2019-06-12 01:40:01 +0200

    x

---
+++ Inleveropdrachten/P5 - JPA OV Chipkaart - Inleveropdracht/hibernate/hu/Log.java
@@ -0,0 +1,35 @@
+package hibernate.hu;
+
+public class Log {
+	private int id;
+	private String message;
+
+	public Log() {
+	}
+
+	public Log(int id, String message) {
+		this.id = id;
+		this.message = message;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "Log{" + "id=" + id + ", message='" + message + '\'' + '}';
+	}
+}
