0d2fc7f5b94ddeaad436fdfc3f001cd368019cf5
murrie030 <43439571+murrie030@users.noreply.github.com>
2019-06-12 10:24:50 +0200

    Update Reiziger.java

--- Inleveropdrachten/P2 - DAO één op veel relatie/Reiziger.java
+++ Inleveropdrachten/P2 - DAO één op veel relatie/Reiziger.java
@@ -1,29 +1,71 @@
 package p2DAO;
 
-import java.sql.Date;
+import java.util.ArrayList;
 
 public class Reiziger {
 	private String naam;
-	private Date gbdatum;
+	private String gbdatum;
+	private ArrayList<OvChipkaart> ovKaarten; 
+	private int reizigerId;
 
-	public Reiziger(String naam, Date gbdatum) {
-		this.naam = naam;
+	public Reiziger() {
+	};
+
+	public Reiziger(String naam, String gbdatum) {
 		this.gbdatum = gbdatum;
+		this.naam = naam;
+		ovKaarten = new ArrayList<OvChipkaart>();
+
 	}
 
-	public String getNaam() {
+	public Reiziger(String naam, String gbdatum, int reizigerId) { 
+		this.naam = naam;
+		this.gbdatum = gbdatum;
+		this.reizigerId = reizigerId;
+		ovKaarten = new ArrayList<OvChipkaart>();
+
+	}
+
+	public void setReizigerId(int reizigerid) {
+		this.reizigerId = reizigerId;
+	}
+
+	public int getReizigerId() {
+		return reizigerId;
+	}
+
+	public void voegKaartToe(OvChipkaart kaart) {
+		ovKaarten.add(kaart);
+		System.out.println("kaart toegevoegd");
+	}
+
+	public ArrayList<OvChipkaart> getovKaarten() {
+		return ovKaarten;
+	}
+
+	public String getNaam() { 
 		return naam;
 	}
 
-	public void setNaam(String nm) {
-		this.naam = nm;
+	public void setNaam(String naam) {
+		this.naam = naam;
 	}
 
-	public Date getGBdatum() {
+	public String getGBdatum() { 
 		return gbdatum;
 	}
 
-	public void setGBdatum(Date dt) {
-		this.gbdatum = dt;
+	public void setGBdatum(String gbdatum) { 
+		this.gbdatum = gbdatum;
+	}
+	
+	public void voegOvToe(OvChipkaart ov) {
+		if (!this.ovKaarten.contains(ov)) {
+			this.ovKaarten.add(ov);
+		}
+	}
+	public String toString() {
+		String s = "Naam: " + naam + " , Geboortedatum: " + gbdatum;
+		return s;
 	}
 }
